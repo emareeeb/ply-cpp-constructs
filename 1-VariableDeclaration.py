52430351b282963972eb9671124a0cbdf325240f
Areeb Ahmed <132186043+emareeeb@users.noreply.github.com>
2023-11-17 19:01:50 +0530

    user input addition

--- 1-VariableDeclaration.py
+++ 1-VariableDeclaration.py
@@ -11,19 +11,18 @@ tokens = (
     'FLOAT',
 )
 
-# t_TYPE = r''
-# t_ASSIGNMENT = r'='
 t_SEMICOLON = r';'
 t_ignore = r' '
 
 def t_TYPE(t):
-    r'int\s| char | float | double'
+    r'\b(int|char|float|double)\b'
     return t
 
 def t_IDENTIFIER(t):
     r'[a-zA-Z0-9._><\-][a-zA-Z0-9._><\-]*'
     t.type = 'IDENTIFIER'
     return t
+
 def t_ASSIGNMENT(t):
     r'='
     return t
@@ -36,21 +35,15 @@ def t_error(t):
     print(f"Illegal character '{t.value[0]}'")
     t.lexer.skip(1)
 
-
 lexer = lex.lex()
 
 # Parser
 def p_variable_declaration(p):
     '''variable_declaration : TYPE IDENTIFIER ASSIGNMENT expression SEMICOLON'''
-    p[0] = (p[1], p[2], p[3], p[5])
+    p[0] = "Valid Variable Declaration"
 
-# def  p_type(p):
-#     ''' type: INTEGER
-#               FLOAT'''
-        
 def p_expression(p):
     '''expression : IDENTIFIER'''
-                   
     p[0] = p[1]
 
 def p_error(p):
@@ -58,12 +51,6 @@ def p_error(p):
 
 parser = yacc.yacc()
 
-# Test the parser
-input_code = '''float x=5.5;'''
-lexer.input(input_code)
-
-for i in lexer:
-    print(i)
-
+input_code = input("Enter your syntax : ")
 result = parser.parse(input_code)
 print(result)
